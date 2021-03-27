2363a9c170aa25968c41a412fbd51db6013ffc05
Alex Badamtchi <alex.badamtchi@gmail.com>
2021-03-27 21:54:06 +0300

    Delete Linked List.py

--- Linked List.py
+++
@@ -1,8 +0,0 @@
-class Node:
-    def __init__(self, d, n=None, p=None):
-        self.data = d
-        self.nxt = n
-        self.prv = p
-
-    def __str__(self):
-        return ('(%s)' % self.data)
